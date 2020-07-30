c46e06f846eddd6395aa952bdca879abca8ea9d3
Melissa <melissakheying@gmail.com>
2020-07-30 18:37:53 -0500

    Add buttons array, create renderButtons function, call render function

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -7,7 +7,40 @@ export default class Input extends React.Component {
     currentStudent: '',
   }
 
+  buttons = [
+    {
+      buttonTxt: 'Enter Student'
+    },
+    {
+      buttonTxt: 'Spin the Wheel'
+    },
+  ]
 
+  renderButtons = () => {
+    this.buttons.map((button, i) => {
+      return (
+        <Button
+        buttonHandler={this.updateName}
+        buttonTxt={button.buttonTxt}
+        key={i}
+        />
+      )
+    })
+  }
 
+  updateName = ({target}) => {
+    this.setState(name => {
+      return {currentStudent: name.currentStudent}
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.currentStudent}</p>
+        {this.renderButtons()}
+      </div>
+    )
+  }
 
 }
