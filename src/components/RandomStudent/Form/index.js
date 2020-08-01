64b332a90e03dc90771c7b4a178331f1a946fe1b
Melissa <melissakheying@gmail.com>
2020-08-01 17:30:08 -0500

    Set up event handler for submit on form, Set up prop types

--- src/components/RandomStudent/Form/index.js
+++ src/components/RandomStudent/Form/index.js
@@ -1,3 +1,27 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
+export const Form = () => {
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    formHandler(event)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" />
+      <button>Add Student</button>
+    </form>
+  )
+  }
+
+  Form.defaultProps = {
+    value: '',
+  }
+
+  Form.propTypes = {
+    formHandler: PropTypes.func,
+    value: PropTypes.string,
+  }
